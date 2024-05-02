7021996fd1b2a71f3d45a3294e7a58c08c51c6a4
한소영 <fjw_1010@naver.com>
2024-05-02 15:52:07 +0900

    ES6
    ES6을 사용 전에 1주차 때 했던 개념들을 복습한 내용

---
+++ 2주차/01.js
@@ -0,0 +1,31 @@
+/*
+    ES6
+
+    2015년도 이전 => var
+    ES6 => let(변수), const(상수)
+    let과 const의 차이점은 다시 '선언'할 수 있느냐, 없느냐
+    let a = 10;
+    a = 20;
+    console.log(a) << 재할당 가능
+    let a = 20; << 불가능
+    const b = 10;
+    b = 10;
+    console.log(b) << 재할당 불가능
+    ==========================================================
+    function add() {}
+
+    var add = function () {};
+    var add = () => {
+        return 1;
+    };
+    var add = (x) => 1;
+    삼항 연산자
+    conditon ? true인 경우 : false인 경우
+    console.log(true ? '참' : '거짓');
+    console.log(false ? '참' : '거짓');
+
+    console.log(true ? "참" : "거짓");
+    console.log(false ? "참" : "거짓");
+    console.log(1 === 1 ? "참" : "거짓");
+    console.log(1 !== 1 ? "참" : "거짓");
+*/
